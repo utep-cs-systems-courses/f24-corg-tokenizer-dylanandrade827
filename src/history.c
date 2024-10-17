209c6dce8af070d06f2e81140041e470eb3e50ce
dylanandrade827 <dylanandrade827@gmail.com>
2024-10-17 01:19:46 -0600

    finalized

--- src/history.c
+++ src/history.c
@@ -3,10 +3,62 @@
 #include "tokenizer.h"
 #include "history.h"
 
-int sLength(char *str){
-  int len = 0;
-  while(str[len] != 0){
-    len++;
+List *init_history(){
+  List *list = malloc(sizeof(List));
+  list->root = malloc(sizeof(Item));
+
+  return list;
+}
+
+void add_history(List *list, char *str){
+  int id = 1;
+  Item *hold = list->root;
+
+  while (hold->next != 0){
+    hold = hold->next;
+    id++;
   }
-  return len;
+
+  hold->next = malloc(sizeof(Item));
+
+  int i = 0;
+  while(str[i] != 0){
+    i++;
+  }
+  hold->next->str = copy_str(str, i);
+  hold->next->id = id;
+}
+
+char *get_history(List *list, int id){
+  Item *hold = list->root;
+
+  while(hold != 0){
+    if(hold->id == id){
+      return hold->str;
+    }
+    hold = hold->next;
+  }
+  return "No history available:(";
+}
+
+void print_history(List *list){
+  Item *hold = list->root->next;
+  
+  while(hold !=0){
+    printf("%d %s\n", hold->id, hold->str);
+    hold = hold->next;
+  }
+}
+
+void free_history(List *list){
+  Item *curr = list->root;
+  
+  while(curr != NULL){
+    Item *last = curr;
+    
+    curr = curr->next;
+    free(last->str);
+    free(last);
+  }
+  free(list);
 }
--- src/uimain.c
+++ src/uimain.c
@@ -7,27 +7,65 @@ int main()
 {
   char str[1000];
   char exit[4] = "exit";
-
+  char history[7] = "history";
+  char * copy;
+  List *list = init_history();
   int i = 0;
+  int id = 0;
   
  top:
   printf("$ ");
   fgets(str, sizeof(str), stdin);
 
- check:
-  if (str[i] == exit[i])
-    {
-      if (i == 3 && str[4] == '\n')
-	{
-	  goto bye;
-	}
-      i++;
-      goto check;
-    }
-  i = 0;
-  printf("%s", str);
+  if (str[i] == exit[i]){
+    i = 0;
+    goto checkExit;
+  }
+
+  if (str[i] == history[i]){
+    i = 0;
+    goto checkHistory;
+  }
+
+  if (str[i] == '!'){
+    i = 0;
+    goto getHistory;
+  }
+  goto skip;
+      
+ checkExit:
+  if (str[i] != exit[i]) goto skip;
+  if (i == 3 && str[4] == '\n') goto bye;
+  i++;
+  goto checkExit;
+  
+ checkHistory:
+  if (str[i] != history[i]) goto skip;
+  if (i == 6 && str[7] == '\n') goto history;
+  i++;
+  goto checkHistory;
+
+ getHistory:
+  id = str[i+1] - '0';
+  char * get = get_history(list, id);
+  printf("%s", get);
+  goto top;
+  
+ skip:
+  copy = copy_str(str, sizeof(str));
+  add_history(list, copy);
+  char ** tokenizer = tokenize(copy);
+  print_tokens(tokenizer);
+  
+  goto top;
+
+ history:
+  print_history(list);
   goto top;
   
  bye:
+  free_tokens(tokenizer);
+  free_history(list);
   return 0;
 }
+B
